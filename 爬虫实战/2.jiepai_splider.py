4e3ac2273cb82b7d7b4c67dee35fcac24ff5c8d6
kuaikang <359405466@qq.com>
2018-02-24 00:11:33 +0800

    街拍

---
+++ 爬虫实战/2.jiepai_splider.py
@@ -0,0 +1,20 @@
+import requests,json
+def get_page_index():
+    params = {
+        'offset': '0',
+        'format': 'json',
+        'keyword': '街拍',
+        'autoload':'true',
+        'count': '20',
+        'cur_tab':'1',
+        'from':'search_tab'
+    }
+    resp = requests.get("https://www.toutiao.com/search/",params=params)
+    return resp.text
+
+def main():
+    html = get_page_index()
+    print(html)
+
+if __name__ == '__main__':
+    main()
