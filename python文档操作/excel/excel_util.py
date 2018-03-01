801d1183a34542d9010065b8e57e43ffa2b7e75d
kuaik <359405466@qq.com>
2018-03-01 17:54:37 +0800

    操作excel

--- python文档操作/excel/excel_util.py
+++ python文档操作/excel/excel_util.py
@@ -1,5 +1,5 @@
 from openpyxl import load_workbook
-
+from openpyxl.workbook import Workbook
 
 def get_excel(filepath):
     '''返回excel中内容,以列表的形式返回'''
@@ -23,7 +23,16 @@ def write_file(data,filename="a.txt"):
     with open(filename,mode="w",encoding="utf-8") as f:
         f.write(data)
 
+def create_excel(list,file="a.xlsx"):
+    wb = Workbook()
+    ws = wb.worksheets[0]
+    ws.title = "sheet1"
+    for i in range(len(list)):
+        for j in range(len(list[i])):
+            ws.cell(row=i+1,column=j+1,value=list[i][j])
+    wb.save(file)
+
 if __name__ == '__main__':
     data = get_excel("test.xlsx")
     print(data)
-
+    create_excel(data)
