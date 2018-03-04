284dfe33c45b5cb36fccc60db7a0e9d070b55919
kuaikang <359405466@qq.com>
2018-03-04 22:30:30 +0800

    爬取猫眼

--- 4.文件操作/3.read()方法.py
+++ 4.文件操作/3.read()方法.py
@@ -1,21 +1,30 @@
-print("read()".center(50,"-"))
-f = open("test.txt","r",encoding="utf-8")
-print(f.read(14)) # 10表示从文件中读取的字节数.不填则表示读取所有
-f.close()
+import json
 
-print("readline()".center(50,"-"))
-f = open("test.txt","r",encoding="utf-8")
-print(f.readline().strip())
-print(f.readline())
-f.close()
+# print("read()".center(50, "-"))
+# f = open("test.txt", "r", encoding="utf-8")
+# print(f.read(14))  # 10表示从文件中读取的字节数.不填则表示读取所有
+# f.close()
+#
+# print("readline()".center(50, "-"))
+# f = open("test.txt", "r", encoding="utf-8")
+# print(f.readline().strip())
+# print(f.readline())
+# f.close()
+#
+# print("readlines()".center(50, "-"))
+# f = open("test.txt", "r", encoding="utf-8")
+# data = f.readlines()  # 得到的是一个列表,每行是一个元素
+# print(data)
+# f.close()
+#
+# f = open("test.txt", "r", encoding="utf-8")
+# for line in f:
+#     print(line.strip())
+# f.close()
 
-print("readlines()".center(50,"-"))
-f = open("test.txt","r",encoding="utf-8")
-data = f.readlines() # 得到的是一个列表,每行是一个元素
-print(data)
-f.close()
-
-f = open("test.txt","r",encoding="utf-8")
+# 读取text文件中的json数据
+f = open("test.txt", mode="r", encoding="utf8")
 for line in f:
-    print(line.strip())
+    data = json.loads(line.strip())
+    print(type(data))
 f.close()
