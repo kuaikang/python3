366da26fcc9eda4f0664e8f191c2b434a931c0c5
kuaik <359405466@qq.com>
2018-03-05 08:57:54 +0800

    xuele

--- 爬虫实战/3.xuele.py
+++ 爬虫实战/3.xuele.py
@@ -24,7 +24,6 @@ def get_subject():
     res = browser.execute_script('$.ajax({type: "POST",url:"http://www.xueleyun.com/member/book/selectSubjectBySchool.ajax",success: function(data) {$("body").append({"schoolId":"10001"});},beforeSend: function(xhr) {xhr.setRequestHeader("Cookie", "SESSION=8E69F1E229E02A63D9E21A2CC016D78C");}});')
     print(res)
 
-
 def main():
     login()
     get_subject()
