f7ee8ee9116ed56bff5323ea021fe88f6b221e9b
kuaikang <359405466@qq.com>
2018-03-14 00:18:04 +0800

    21世纪教育爬取

---
+++ 爬虫实战/resource/21世纪教育/分析.py
@@ -0,0 +1,19 @@
+# 课本请求地址:
+#     http://tiku.21cnjy.com/tiku.php?mod=quest
+#     参数:
+#         root: 10431
+#         additional: yeah: Tue, 13 Mar 2018 14:43:24 GMT
+#         op: ajaxcatid
+
+# 初中:
+    # 语文  http://tiku.21cnjy.com/tiku.php?mod=quest&channel=2&xd=2
+    # 数学  http://tiku.21cnjy.com/tiku.php?mod=quest&channel=3&xd=2
+    # 英语  http://tiku.21cnjy.com/tiku.php?mod=quest&channel=4&xd=2
+    # 物理  http://tiku.21cnjy.com/tiku.php?mod=quest&channel=6&xd=2
+    # 化学  http://tiku.21cnjy.com/tiku.php?mod=quest&channel=7&xd=2
+    # 历史  http://tiku.21cnjy.com/tiku.php?mod=quest&channel=8&xd=2
+    # 政治思品    http://tiku.21cnjy.com/tiku.php?mod=quest&channel=9&xd=2
+    # 地理  http://tiku.21cnjy.com/tiku.php?mod=quest&channel=10&xd=2
+    # 生物  http://tiku.21cnjy.com/tiku.php?mod=quest&channel=11&xd=2
+
+# 题型 type 1.单选
