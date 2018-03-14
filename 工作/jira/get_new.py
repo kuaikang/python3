fbf550cb22eca34d5aa0fd9d21ce50c785105a2b
kuaikang <359405466@qq.com>
2018-03-15 00:14:34 +0800

    21世纪教育

--- 工作/jira/get_new.py
+++ 工作/jira/get_new.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from bs4 import BeautifulSoup
 import time
 from 工作.jira import send_message
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     browser.get("http://192.168.121.155:8080")
     time.sleep(2)
-    user = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#login-form-username")))
+    user = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "#login-form-username")))
     user.send_keys("蒯康")
     browser.find_element(By.CSS_SELECTOR, "#login-form-password").send_keys("111111")
     browser.find_element(By.CSS_SELECTOR, "#login").click()
@@ -24,14 +24,14 @@ if __name__ == '__main__':
         soup = BeautifulSoup(browser.page_source, "lxml")
         lis = soup.find(attrs={"class": "list-content"}).select("li")
         first = lis[0]
-        count = 0
         while True:
-            count = count + 1
             time.sleep(60)
             browser.refresh()
             soup = BeautifulSoup(browser.page_source, "lxml")
             lis_new = soup.find(attrs={"class": "list-content"}).select("li")
-            if lis_new[0] != first:
-                send_message.send_message(lis_new[0].attrs["data-key"], lis_new[0].attrs["title"], "1074609185@qq.com")
-                print(lis_new[0].attrs["data-key"], lis_new[0].attrs["title"])
-                first = lis_new[0]
+            new = lis_new[0]
+            if new != first:
+                send_message.send_message(new.attrs["data-key"], new.attrs["title"], "1074609185@qq.com")
+                send_message.send_message(new.attrs["data-key"], new.attrs["title"], "359405466@qq.com")
+                print(new.attrs["data-key"], new.attrs["title"])
+                first = new
--- 爬虫实战/resource/21世纪教育/base.py
+++ 爬虫实战/resource/21世纪教育/base.py
@@ -1,5 +1,7 @@
-import requests, json, re
+import requests, json, re, threading
 from bs4 import BeautifulSoup
+from 数据库存储.Mysql import pymysql_util
+import time
 
 cz_dic = {"2": "语文", "3": "数学", "4": "英语", "6": "物理", "7": "化学", "8": "历史", "9": "政治思品", "10": "地理", "11": "生物"}
 
@@ -30,17 +32,43 @@ def get_book(version_id):
     return result
 
 
+def get_unit(book_id):
+    units = get_book(book_id[0])
+    if units:
+        count = 1
+        sql = 'INSERT INTO unit (unit_id, unit_name, book_id, num) VALUES ("%s", "%s", "%s", %s)'
+        data = []
+        for unit in units:  # ('22783', '第一单元 成长的足迹')
+            u = [unit[0], unit[1], book_id[0], count]
+            data.append(u)
+            count += 1
+        pymysql_util.insert_many(db, sql, data=data)
+        time.sleep(1)
+
+
 if __name__ == '__main__':
-    for s in cz_dic.keys():
-        versions = get_version(xd='2', channel=s)
-        for item in versions:  # ['语文', '2', '北师大版', '10426']
-            books = get_book(item[-1])
-            for book in books:  # '1436','七年级上册（2016）'
-                item.append(book[1])
-                item.append(book[0])
-                units = get_book(book[0])
-                if units:
-                    for unit in units:  # ('22783', '第一单元 成长的足迹')
-                        chapters = get_book(unit[0])
-                        if chapters:
-                            print(chapters)
+    db = pymysql_util.get_db()
+    # for s in cz_dic.keys():
+    #     versions = get_version(xd='2', channel=s)
+    #     for item in versions:  # ['语文', '2', '北师大版', '10426']
+    #         # pymysql_util.insert_one(db, "INSERT INTO editor (edition_id, edition_name) VALUES ('%s','%s')" % (
+    #         #     str(item[3]), item[2]))
+    #         books = get_book(item[-1])
+    #         for book in books:  # '1436','七年级上册（2016）'
+    #             # sql = "INSERT INTO book (book_id, book_name, grade_id, edition_id, edition_name) VALUES ('%s', '%s', '%s', '%s', '%s')"
+    #             # pymysql_util.insert_one(db, sql % (book[0], book[1], '2', item[3], item[2]))
+    #             units = get_book(book[0])
+    #             if units:
+    #                 count = 1
+    #                 for unit in units:  # ('22783', '第一单元 成长的足迹')
+    #                     sql = "INSERT INTO unit (unit_id, unit_name, book_id, num) VALUES ('%s', '%s', '%s', %s)"
+    #                     pymysql_util.insert_one(db, sql % (unit[0], unit[1], book[0], count))
+    #                     count += 1
+    #                     # chapters = get_book(unit[0])
+    #                     # if chapters:
+    #                     #     print(chapters)
+
+    books = pymysql_util.find_all(db, "select book_id from book")
+    for book_id in books:
+        t = threading.Thread(target=get_unit, args=(book_id,))
+        t.start()
