176a3c10a3895b2987dff0ea35ae10b6b533235b
kuaikang <359405466@qq.com>
2018-03-20 23:50:55 +0800

    爬取猫眼

--- 第三方模块/Selenium库使用/headless/base.py
+++ 第三方模块/Selenium库使用/headless/base.py
@@ -1,32 +1,18 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('Cookie')
-driver = webdriver.Chrome(chrome_options=chrome_options)
 
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--headless')  # 不显示浏览器,静默模式
+# chrome_options.add_argument('disable-infobars')
 
-def write_unit(browser, categories, bookversion, nianji, chid, xd):
-    url = "http://www.zujuan.com/question?categories=%s&bookversion=%s&nianji=%s&chid=%s&xd=%s"
-    browser.get(url % (categories, bookversion, nianji, chid, xd))
-    f = open("a.txt", "w", encoding="utf8")
-    f.write(browser.page_source)
-    f.close()
-
-
-def insert_unit(file_name, bookversion):
-    f = open("a.txt", mode="r", encoding="utf8")
-    html = f.read()
-    f.close()
-    soup = BeautifulSoup(html, "lxml")
-    tree = soup.find_all(attrs={"id": "J_Tree"})
-    ts = tree[0].find_all("a")
-    ids = html[html.find("fetchTree"):html.find("function fetchTree")]
-    ids = ids[ids.find("[{") + 1:ids.find("]")]
-    ids = ids.split(":")
-    f = open(file_name, mode="a", encoding="utf8")
-    for i in range(1, len(ids), 3):
-        f.write(bookversion + "," + ts[(i - 1) // 3].get_text().strip() + "," + ids[i].split(",")[0][1:-1])
-        f.write("\n")
-    f.close()
+if __name__ == '__main__':
+    driver = webdriver.Chrome(chrome_options=chrome_options, desired_capabilities=None)
+    driver.get("http://www.baidu.com")
+    element = driver.find_element_by_id("#id")
+    if element:
+        element.get_attribute("src")  # 获取元素属性
+        element.text  # 获取元素文本
+    # driver.maximize_window()  # 浏览器最大化
+    # driver.set_window_size(100, 200)  # 宽高
+    # driver.refresh()  # 刷新页面
+    # driver.close()  # 关闭当前页面
+    driver.quit()  # 关闭当前窗口并结束进程
