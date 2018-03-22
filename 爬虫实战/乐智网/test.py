eaa22a4d0cbcaa166e92b9e18fb5b36fd2292214
kuaikang <359405466@qq.com>
2018-03-23 00:56:10 +0800

    爬取猫眼

--- 爬虫实战/乐智网/test.py
+++ 爬虫实战/乐智网/test.py
@@ -3,7 +3,11 @@ from urllib.parse import urlencode
 
 if __name__ == '__main__':
     data = {
-
+        "gradeId":"G04",
+        "courseId":"K01",
+        "versionId":"V02",
+        "volumeId":"1296"
     }
-    resp = requests.get("http://www.jiaoxueyun.cn/resources-more-inter!getTrees.do?")
+    resp = requests.post("http://www.jiaoxueyun.cn/resources-more-inter!getTrees.do?"+urlencode(data))
     print(resp.text)
+
