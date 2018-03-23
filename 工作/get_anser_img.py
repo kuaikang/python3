448ec6ff4aafcc05b193acaf4a24aa7c23e5394f
kuaikang <359405466@qq.com>
2018-03-23 21:16:34 +0800

    爬取猫眼

--- 工作/get_anser_img.py
+++ 工作/get_anser_img.py
@@ -8,8 +8,8 @@ def get_db():
     # 打开数据库连接
     try:
         db = pymysql.connect(
-            host="192.168.121.40", user="root",
-            password="001233", db="kuaik", port=3306,
+            host="localhost", user="root",
+            password="123456", db="resource", port=3306,
             charset="utf8"
         )
         return db
@@ -17,10 +17,11 @@ def get_db():
         print(e)
 
 
-def get_question_ids(cur,book_id,index):
-    sql = "SELECT qc.question_id,q.answer_url from chapter_question qc LEFT JOIN chapter c on qc.chapter_id = c.chapter_id " \
-          "LEFT JOIN question q on qc.question_id = q.question_id WHERE book_id = '%s' " \
-          "and q.answer_url is null GROUP BY qc.question_id LIMIT %s,20"%(book_id,index)
+def get_question_ids(cur, index):
+    # sql = "SELECT qc.question_id,q.answer_url from chapter_question qc LEFT JOIN chapter c on qc.chapter_id = c.chapter_id " \
+    #       "LEFT JOIN question q on qc.question_id = q.question_id WHERE book_id = '%s' " \
+    #       "and q.answer_url is null GROUP BY qc.question_id LIMIT %s,20"%(book_id,index)
+    sql = "SELECT question_id from question WHERE answer_url is null LIMIT %s,300" % index
     cur.execute(sql)
     return cur.fetchall()
 
@@ -37,10 +38,10 @@ def update_question(db, cur, answer_url, question_id):
     db.commit()
 
 
-def main(book_id,index):
+def main(index):
     db = get_db()
     cur = db.cursor()
-    question_ids = get_question_ids(cur, book_id,index)
+    question_ids = get_question_ids(cur, index)
     pattern = re.compile('.*?"answer":"(.*?)>*?"', re.S)
     sql = "UPDATE question SET answer_url = '{answer_url}' WHERE question_id = '{question_id}'"
     print("start")
@@ -53,18 +54,20 @@ def main(book_id,index):
         resp.close()
         cur.execute(sql.format(answer_url=src, question_id=question[0]))
         db.commit()
-        time.sleep(0.2)
+        time.sleep(0.3)
     cur.close()
     db.close()
 
 
 if __name__ == '__main__':
-    start = time.time()
-    task = []
-    for i in range(6):
-        t = threading.Thread(target=main, args=("119441",i*20,))
-        t.start()
-        task.append(t)
-    for t in task:
-        t.join()
-    print(time.time() - start)
+    # start = time.time()
+    # task = []
+    # for i in range(5):
+    #     t = threading.Thread(target=main, args=(i * 300,))
+    #     t.start()
+    #     task.append(t)
+    # for t in task:
+    #     t.join()
+    # print(time.time() - start)
+    for i in range(30):
+        main(0)
--- 工作/zujuan/question_spider.py
+++ 工作/zujuan/question_spider.py
@@ -7,8 +7,8 @@ def get_db():
     # 打开数据库连接
     try:
         db = pymysql.connect(
-            host="192.168.121.40", user="root",
-            password="001233", db="kuaik", port=3306,
+            host="localhost", user="root",
+            password="123456", db="resource", port=3306,
             charset="utf8"
         )
         return db
@@ -33,7 +33,7 @@ def get_question(categories, page):
     grade8 = {"grade_id[]": "8"}
     grade9 = {"grade_id[]": "9"}
     head = {
-        "Cookie":"_ga=GA1.2.1209185538.1520329414; _gid=GA1.2.1039098456.1521423702; PHPSESSID=jp4v0ul3nhsh566c486nkfvcg2; xd=75519cb9f2bf90d001c0560f5c40520062a60ada9cb38350078f83e04ee38a31a%3A2%3A%7Bi%3A0%3Bs%3A2%3A%22xd%22%3Bi%3A1%3Bi%3A2%3B%7D; _csrf=2af821a7165551239a7c41d1646643aca3c90ac2d8663bf01dcbe75e9cb2f470a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22b9AxHw7NBLZlWMlKCQEPoDVnDGY9m4Xr%22%3B%7D; isRemove=1; Hm_lvt_6de0a5b2c05e49d1c850edca0c13051f=1521423702,1521446251,1521509816,1521515834; chid=19e92f27f767d4d8eceab9ca12975b67007ccb5366fcb4209a851c120c07f81ca%3A2%3A%7Bi%3A0%3Bs%3A4%3A%22chid%22%3Bi%3A1%3Bs%3A2%3A%2210%22%3B%7D; Hm_lpvt_6de0a5b2c05e49d1c850edca0c13051f=1521518473",
+        "Cookie":"_ga=GA1.2.790539841.1520347247; _gid=GA1.2.1626214821.1521467380; PHPSESSID=1jsplddulmk89dh680u5smbak2; xd=75519cb9f2bf90d001c0560f5c40520062a60ada9cb38350078f83e04ee38a31a%3A2%3A%7Bi%3A0%3Bs%3A2%3A%22xd%22%3Bi%3A1%3Bi%3A2%3B%7D; _csrf=f2d9eb82584b41291f73a2853d6e94dffcee961ed60e63ec50a3cc3a605575c6a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22vzrfNO2EeDe1z6SToEZaY3YUVXdsAyvR%22%3B%7D; isRemove=1; Hm_lvt_6de0a5b2c05e49d1c850edca0c13051f=1521302134,1521467380,1521559971,1521630723; chid=cfb34d37b40eefc2ccbe518532b4834f2a7aa0aa8db56dff2d2fd9a9a5c919dda%3A2%3A%7Bi%3A0%3Bs%3A4%3A%22chid%22%3Bi%3A1%3Bs%3A1%3A%226%22%3B%7D; Hm_lpvt_6de0a5b2c05e49d1c850edca0c13051f=1521635802",
         "X-CSRF-Token": "YuvZ0usGfrQHZJ9QxC4RghO0y-1dTrxnimhxKsm4w6QA0piqo3FJ-kUoxTyTY33JUOWOvTIK6gnOLygTpIyb1g==",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36"
     }
@@ -64,15 +64,15 @@ def main(book_id):
     chapter_ids = cur.fetchall()
     print(chapter_ids)
     print("start")
-    sql_q = "INSERT INTO `kuaik`.`question` (`question_id`, `context`, `type`, `difficult`) VALUES ('{0}', '{1}', '{2}', '{3}')"
-    sql_cq = 'INSERT INTO `kuaik`.`chapter_question` (`id`,`chapter_id`, `question_id`) VALUES (UUID(),"{0}", "{1}");'
-    sql_t = 'INSERT INTO `kuaik`.`tag` (`tag_id`, `tag_name`, `question_id`) VALUES (UUID(), "{0}", "{1}");'
+    sql_q = "INSERT INTO question (`question_id`, `context`, `type`, `difficult`) VALUES ('{0}', '{1}', '{2}', '{3}')"
+    sql_cq = 'INSERT INTO `chapter_question` (`id`,`chapter_id`, `question_id`) VALUES (UUID(),"{0}", "{1}");'
+    sql_t = 'INSERT INTO tag (`tag_id`, `tag_name`, `question_id`) VALUES (UUID(), "{0}", "{1}");'
     for line in chapter_ids:
         # f = open("cz_chapter.txt",mode="r",encoding="utf8")
         # for line in f.readlines()[301:600]:
         #     line = line.split(",")
         for i in range(1, 100):
-            if i == 9:break
+            if i == 30:break
             data, total = get_question(line[0], i)
             if not data: break
             page = (total + 10 - 1) // 10
@@ -88,19 +88,19 @@ def main(book_id):
                     try:
                         cur.execute(sql_cq.format(line[0], q.get("question_id")))
                         cur.execute(
-                            "SELECT * FROM `kuaik`.`question` WHERE question_id = '%s'" % q.get("question_id"))
+                            "SELECT * FROM question WHERE question_id = '%s'" % q.get("question_id"))
                         if not cur.fetchone():
                             cur.execute(
                                 sql_q.format(q.get("question_id"), pymysql.escape_string(q.get("question_text")),
                                              q.get("question_type"),
                                              q.get("difficult_index")))
                             for key in q.get("options").keys():
-                                sql_i = 'INSERT INTO `kuaik`.`item` (`item_id`, `content`, `option`,`question_id`) VALUES (UUID(), "{0}", "{1}","{2}");'
+                                sql_i = 'INSERT INTO item (`item_id`, `content`, `option`,`question_id`) VALUES (UUID(), "{0}", "{1}","{2}");'
                                 cur.execute(sql_i.format(pymysql.escape_string(q.get("options").get(key)), key,
                                                          q.get("question_id")))
                             cur.execute(sql_t.format(q.get("knowledge"), q.get("question_id")))
                         db.commit()
-                        time.sleep(0.4)
+                        time.sleep(0.2)
                     except Exception as e:
                         print(e)
     cur.close()
@@ -108,7 +108,7 @@ def main(book_id):
 
 
 if __name__ == '__main__':
-    main("4977")
+    main("83725")
 
 # print(q.get("question_text"))
 # print(q.get("options"))  # 选项
