2d7fc8a573cda51eeb219e15e63eac1f251922a4
kuaikang <359405466@qq.com>
2018-03-25 22:26:04 +0800

    update

--- 爬虫实战/乐智网/test.py
+++ 爬虫实战/乐智网/test.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     for d in dirs:
         for root, dirs, files in os.walk(path + d):  # 遍历统计
             for each in files:
-                size = os.path.getsize(path + d + "/" + each)/1024/1024
+                size = os.path.getsize(path + d + "/" + each) / 1024 / 1024
                 if size > 3:
                     print(size)
                     print(d)
                 count += 1
-    print("数量",count)
+    print("数量", count)
