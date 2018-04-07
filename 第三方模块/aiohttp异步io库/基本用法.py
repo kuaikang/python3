7fe3626ca5be32f8b7be07202b487c0833273432
kuaikang <359405466@qq.com>
2018-04-07 21:07:12 +0800

    update

--- 第三方模块/aiohttp异步io库/基本用法.py
+++ 第三方模块/aiohttp异步io库/基本用法.py
@@ -1,15 +1,20 @@
 import aiohttp
 import asyncio
+import time
 
 
 async def get_github():
     async with aiohttp.ClientSession() as session:
-        async with session.get("https://github.com") as r:
+        async with session.get("http://www.zujuan.com/question/detail-1296078.shtml") as r:
             return await r.text()
 
 
 if __name__ == '__main__':
-    tasks = [get_github()]
+    s = time.time()
+    tasks = []
+    for i in range(20):
+        tasks.append(get_github())
     loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
+    print(time.time()-s)
