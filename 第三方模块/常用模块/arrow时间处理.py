46b042e2ebf719d6a8fd212db8340c1036c07112
kuaikang <359405466@qq.com>
2018-04-24 22:04:05 +0800

    update

--- 第三方模块/常用模块/arrow时间处理.py
+++ 第三方模块/常用模块/arrow时间处理.py
@@ -4,11 +4,5 @@ if __name__ == '__main__':
     now = arrow.now()
     # 获取数据
     print(now.year, now.month, now.day, now.hour, now.minute, now.second, now.week)
-
-    # arrow可以转化多种格式的时间
-    print(arrow.get("2018-01-03"))
-    print(arrow.get("2018.01.03"))
-    print(arrow.get("2018/01/03"))
-    print(arrow.get("01-03-2018", 'MM-DD-YYYY'))
-    print(arrow.get("01-03-2018", 'DD-MM-YYYY'))
-    print(arrow.get("1586782011"))
+    print(now.timestamp)  # 时间戳,精确到秒
+    print(now.format('YYYY-MM-DD HH:mm:ss'))  # 格式化时间
