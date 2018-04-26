daffcdaa249bd11da40b34e7ba7f437deefed1b4
kuaikang <359405466@qq.com>
2018-04-27 00:45:42 +0800

    update

--- common/image_util.py
+++ common/image_util.py
@@ -1,5 +1,5 @@
 from PIL import Image
 import pytesseract
-
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 text = pytesseract.image_to_string(Image.open('E:/python3.6/Lib/site-packages/pytesseract/test.png'), lang='chi_sim')
 print(text)
